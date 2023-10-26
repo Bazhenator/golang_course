98e88b07753d8b508045ff219acd7b6301cd5b38
unknown <flusova.sa@edu.spbstu.ru>
2023-10-26 22:11:06 +0300

    array -> heap

--- Task_2/cmd/file.go
+++ Task_2/cmd/file.go
@@ -1,40 +1,46 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 )
 
-func quickSort(arr []int) {
-	if len(arr) <= 1 {
-		return
-	}
+type IntHeap []int
 
-	pivot := arr[len(arr)/2]
-	left, right := make([]int, 0), make([]int, 0)
-	middle := make([]int, 0)
+// Heap's Interface
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func heapSort(arr []int) {
+	h := &IntHeap{}
+	heap.Init(h)
 
 	for _, v := range arr {
-		if v < pivot {
-			left = append(left, v)
-		} else if v > pivot {
-			right = append(right, v)
-		} else {
-			middle = append(middle, v)
-		}
+		heap.Push(h, v)
 	}
 
-	quickSort(left)
-	quickSort(right)
-
-	copy(arr, left)
-	copy(arr[len(left):], middle)
-	copy(arr[len(left)+len(middle):], right)
+	for i := 0; i < len(arr); i++ {
+		arr[i] = heap.Pop(h).(int)
+	}
 }
 
 // Example of tested file
 func findKthLargest(nums []int, k int) int {
-	//Checking Quick sort
-	quickSort(nums)
+	//Checking Heap sort
+	heapSort(nums)
 	//Cheking if k is out of array's range
 	if k <= len(nums) && k >= 0 {
 		return nums[len(nums)-k]
@@ -43,3 +49,8 @@ func findKthLargest(nums []int, k int) int {
 	}
 	return 0
 }
+
+/*func main() {
+	nums := []int{1, 2, 3, 47, 9, 0}
+	fmt.Print(findKthLargest(nums, 5))
+}*/
