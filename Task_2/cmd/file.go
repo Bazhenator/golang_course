9e62d632f2f2a6e9831a8424842c0ca9b5901e34
unknown <flusova.sa@edu.spbstu.ru>
2023-10-26 20:40:21 +0300

    removing main func

--- Task_2/cmd/file.go
+++ Task_2/cmd/file.go
@@ -15,9 +15,3 @@ func findKthLargest(nums []int, k int) int {
 	}
 	return 0
 }
-
-func main() {
-	nums1 := []int{3, 2, 3, 1, 2, 4, 5, 5, 6}
-	result := findKthLargest(nums1, 4)
-	fmt.Println(result)
-}
