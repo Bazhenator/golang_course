3bb1e690e13a220986b6979b6a1747692d1cf5d6
unknown <flusova.sa@edu.spbstu.ru>
2023-10-26 21:53:27 +0300

    checking quickSort worktime

--- Task_2/cmd/file.go
+++ Task_2/cmd/file.go
@@ -4,21 +4,31 @@ import (
 	"fmt"
 )
 
-// Example of tested file
-func findKthLargest(nums []int, k int) int {
-	//Checking Bubble sort
-	swapped := true
-	for swapped {
-		swapped = false
-		for i := 0; i < len(nums)-1; i++ {
-			if nums[i] > nums[i+1] {
-				c := nums[i]
-				nums[i] = nums[i+1]
-				nums[i+1] = c
-				swapped = true
-			}
+func quickSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	pivot := arr[0]
+	var left, right []int
+
+	for _, v := range arr[1:] {
+		if v <= pivot {
+			left = append(left, v)
+		} else {
+			right = append(right, v)
 		}
 	}
+
+	left = quickSort(left)
+	right = quickSort(right)
+	return append(append(left, pivot), right...)
+}
+
+// Example of tested file
+func findKthLargest(nums []int, k int) int {
+	//Checking Quick sort
+	nums = quickSort(nums)
 	//Cheking if k is out of array's range
 	if k <= len(nums) && k >= 0 {
 		return nums[len(nums)-k]
