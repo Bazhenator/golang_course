aabce336f2bf109f521b1973a72de89724f3dba8
unknown <flusova.sa@edu.spbstu.ru>
2023-10-26 22:16:42 +0300

    checking quickSort worktime 3

--- Task_2/cmd/file.go
+++ Task_2/cmd/file.go
@@ -7,7 +7,6 @@ import (
 
 type IntHeap []int
 
-// Heap's Interface
 func (h IntHeap) Len() int           { return len(h) }
 func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
 func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
@@ -24,33 +23,24 @@ func (h *IntHeap) Pop() interface{} {
 	return x
 }
 
-func heapSort(arr []int) {
-	h := &IntHeap{}
-	heap.Init(h)
+func heapSortDescending(arr []int) {
+	h := IntHeap(arr)
+	heap.Init(&h)
 
-	for _, v := range arr {
-		heap.Push(h, v)
-	}
-
-	for i := 0; i < len(arr); i++ {
-		arr[i] = heap.Pop(h).(int)
+	for i := len(arr) - 1; i >= 0; i-- {
+		arr[i] = heap.Pop(&h).(int)
 	}
 }
 
 // Example of tested file
 func findKthLargest(nums []int, k int) int {
 	//Checking Heap sort
-	heapSort(nums)
+	heapSortDescending(nums)
 	//Cheking if k is out of array's range
 	if k <= len(nums) && k >= 0 {
-		return nums[len(nums)-k]
+		return nums[k-1]
 	} else {
 		fmt.Print("out of range")
 	}
 	return 0
 }
-
-/*func main() {
-	nums := []int{1, 2, 3, 47, 9, 0}
-	fmt.Print(findKthLargest(nums, 5))
-}*/
