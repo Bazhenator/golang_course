fe6a07f8c220cb2563365ba2ff73fa918d0b168e
unknown <flusova.sa@edu.spbstu.ru>
2023-10-26 20:57:19 +0300

    adding ReadMe and some comments

--- Task_2/cmd/file.go
+++ Task_2/cmd/file.go
@@ -7,8 +7,11 @@ import (
 
 // Example of tested file
 func findKthLargest(nums []int, k int) int {
+	//Array sorting with Ints func from sort package
 	sort.Ints(nums)
-	if k <= len(nums) && k > 0 {
+
+	//Cheking if k is out of array's range
+	if k <= len(nums) && k >= 0 {
 		return nums[len(nums)-k]
 	} else {
 		fmt.Print("out of range")
