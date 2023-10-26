366fa4c3d4893513400319a18dc5feec056ed8e8
unknown <flusova.sa@edu.spbstu.ru>
2023-10-26 21:57:56 +0300

    checking quickSort worktime 2

--- Task_2/cmd/file.go
+++ Task_2/cmd/file.go
@@ -4,31 +4,37 @@ import (
 	"fmt"
 )
 
-func quickSort(arr []int) []int {
+func quickSort(arr []int) {
 	if len(arr) <= 1 {
-		return arr
+		return
 	}
 
-	pivot := arr[0]
-	var left, right []int
+	pivot := arr[len(arr)/2]
+	left, right := make([]int, 0), make([]int, 0)
+	middle := make([]int, 0)
 
-	for _, v := range arr[1:] {
-		if v <= pivot {
+	for _, v := range arr {
+		if v < pivot {
 			left = append(left, v)
-		} else {
+		} else if v > pivot {
 			right = append(right, v)
+		} else {
+			middle = append(middle, v)
 		}
 	}
 
-	left = quickSort(left)
-	right = quickSort(right)
-	return append(append(left, pivot), right...)
+	quickSort(left)
+	quickSort(right)
+
+	copy(arr, left)
+	copy(arr[len(left):], middle)
+	copy(arr[len(left)+len(middle):], right)
 }
 
 // Example of tested file
 func findKthLargest(nums []int, k int) int {
 	//Checking Quick sort
-	nums = quickSort(nums)
+	quickSort(nums)
 	//Cheking if k is out of array's range
 	if k <= len(nums) && k >= 0 {
 		return nums[len(nums)-k]
